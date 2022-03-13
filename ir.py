ce1f5bc612959331f0d74692b66d9f7d885223bb
jens <jens.coosemans@student.kuleuven.be>
2022-03-13 14:00:49 +0100

    no message

---
+++ ir.py
@@ -0,0 +1,65 @@
+#code voor de IR sensor
+#receiver: 22
+#emitter: 12
+
+
+import time
+import gpiozero
+from gpiozero import *
+from signal import *
+
+LED_green = LED("BOARD11")
+LED_orange = LED("BOARD13")
+LED_red = LED("BOARD15")
+emitter = LED("BOARD12")
+
+receiver_1 = gpiozero.InputDevice("BOARD22")
+receiver_2 = gpiozero.InputDevice("BOARD36")
+
+
+def turn_off_LEDs():
+    LED_red.off()
+    LED_orange.off()
+    LED_green.off()
+
+
+def check_food_status():
+    while True:
+        emitter.on()
+        time.sleep(1)
+        turn_off_LEDs()
+        state = 0
+        print("LED ON", receiver_1.value)
+        if receiver_1.value == 0:
+            print("More than 66% of food left")
+            state = 1
+            LED_green.on()
+        elif receiver_1.value == 1 and receiver_2.value == 0:
+            print("Between 66% and 33 % of food left")
+            LED_orange.on()
+            state = 2
+        else:
+            print("Less than 33% of food left")
+            LED_red.on()
+            state = 3
+        emitter.off()
+        # return state        #commenten bij testen van de ir sensor
+
+
+
+check_food_status()
+
+
+
+# def test_led():
+#     while True:
+#         LED_green.on()
+#         time.sleep(1)
+#         LED_green.off()
+#         LED_orange.on()
+#         time.sleep(1)
+#         LED_orange.off()
+#         LED_red.on()
+#         time.sleep(1)
+#         LED_red.off()
+# test_led()
---
+++ main.py
@@ -0,0 +1,80 @@
+import time
+
+import mysql.connector
+
+import buzzer
+import motor
+import LDR
+import weight_sensor
+import ir
+import random
+
+mydb = mysql.connector.connect(
+    host="mysql.studev.groept.be",
+    user="a21ib2c03",
+    password="secret",
+    database = "a21ib1c03"
+)
+print(mydb)
+
+mycursor = mydb.cursor()
+
+def test_db():
+    command = "UPDATE bird_feeder SET last_measured = 5 WHERE id = 'weight_sensor' "
+    mycursor.execute(command)
+    mydb.commit()
+test_db()
+def update(sensor_id, sensor_value):
+    # updates last measured value
+    command = "UPDATE bird_feeder SET last_measured = current_measured WHERE id = sensor_id"
+    mycursor.execute(command)
+    mydb.commit()
+
+    # updates current measured value
+    command = "UPDATE bird_feeder SET current_measured = %s WHERE id = %s"
+    val = (sensor_value, sensor_id)
+    mycursor.execute(command, val)
+    mydb.commit()
+
+
+def main():
+    index = 0
+    while True:
+        # check whether there is light outside, if so, we can check the other sensors
+        light_status = LDR.get_LDR_value()
+        update('LDR', light_status)
+        if light_status == 1:
+            if index == 5:
+                # update ir_sensor
+                food_val = ir.check_food_status()
+                update('ir_sensor', food_val)
+
+            # update weight_sensor
+            val = weight_sensor.get_value()
+            update('weight_sensor', val)
+            allowed = weight_sensor.allowed_bird(val,0,200)
+            #we still need a way to acces this value, eventually in the birds database
+            if allowed == 1:
+                last_motor_state = "SELECT * from last_measured WHERE id = 'motor'" #0 or 1, closed and open
+
+                if last_motor_state == 0:
+                    motor.open_hatch()
+                    update('motor', allowed)
+            else:
+                buzzer_on = "SELECT * from  WHERE component_on WHERE id = 'speaker'"
+                if buzzer_on == 1:
+                    buzzer.turn_on()
+                last_motor_state = "SELECT * from last_measured WHERE id = 'motor'"
+                if last_motor_state == 1:
+                    motor.close_hatch()
+                    update('motor', not allowed)
+
+            index += 1
+            time.sleep(1)
+            if buzzer_on ==1:
+                buzzer.turn_off()
+        else:
+            time.sleep(5)
+
+
+main()
